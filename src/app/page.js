3b635d37e27f301b20082b850ec80ded32774db8
Sultan-Ubiquitous <azimakhorajiya@gmail.com>
2025-01-30 23:45:11 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,99 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+export default function page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-[1920px] h-[3413.33px] relative bg-white  overflow-hidden">
+    <div className="w-[1923.52px] h-[1309.31px] left-[-1.08px] top-[-252.19px] absolute">
+        <div className="w-[1923.52px] h-[1134.90px] left-0 top-[174.41px] absolute bg-neutral-900" />
+        <img className=" pt-52 w-[1059.08px] h-[1209.31px] left-[864.45px] top-0 absolute" src="/nigro.png" />
+        <div className="left-[199.68px] top-[326.06px] absolute text-white text-[40.48px] font-bold font-['Inter']">PROTOFILO.</div>
+        <div className="w-[732.34px] h-[646.78px] left-[176.93px] top-[547.23px] absolute">
+            <div className="left-0 top-0 absolute text-white text-[79.34px] font-semibold font-['Inter']">MY NAME IS</div>
+            <div className="left-0 top-[73.93px] absolute text-white text-[99.94px] font-semibold font-['Inter']">VAIBHAV</div>
+            <div className="w-[727.29px] h-[109.66px] left-[5.06px] top-[537.12px] absolute">
+                <div className="w-[727.29px] h-[109.66px] left-0 top-0 absolute bg-[#fdf202] shadow-[0px_6.450417995452881px_17.7386474609375px_0px_rgba(0,0,0,0.37)]" />
+                <div className="left-[62.89px] top-[25.80px] absolute text-[#0b0a00] text-5xl font-bold font-['Inter']">I’M A GRAPHIC DESIGNER</div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+    <div className="w-[1921px] h-[825.27px] left-[0.18px] top-[1057.12px] absolute">
+        <div className="w-[1921px] h-[825.27px] left-0 top-0 absolute bg-[#ececec]" />
+        <div className="w-[530.47px] h-[592.88px] left-[104.90px] top-[115.01px] absolute">
+            <div className="w-[474.06px] h-[532.87px] left-0 top-0 absolute bg-[#202020]" />
+            <div className="w-[465.66px] h-[536.47px] left-[64.81px] top-[56.41px] absolute bg-[#5a5a5a] flex-col justify-center items-center inline-flex overflow-hidden">
+                <img className="w-[540.07px] h-[539.37px]" src="/suvar.png" />
+            </div>
+            <div className="w-[68.41px] h-[43.21px] left-[380.41px] top-[35.39px] absolute">
+                <div className="w-[43.21px] h-[43.21px] left-[25.20px] top-0 absolute bg-[#eeed00]/0 rounded-full border-8 border-white" />
+                <div className="w-[19.20px] h-[19.20px] left-0 top-[10.80px] absolute bg-white rounded-full border-white" />
+            </div>
+        </div>
+    </div>
+    <div className="w-[1607.57px] h-[522.19px] left-[140.47px] top-[1934.21px] absolute">
+        <div className="w-[644.55px] h-[76px] left-0 top-[27.80px] absolute">
+            <div className="w-[577.56px] h-[22.75px] left-[66.98px] top-[37.91px] absolute bg-[#fef102]" />
+            <div className="left-0 top-0 absolute text-[#222023] text-[62.39px] font-extrabold font-['Inter']">WORK EXPERIENCE</div>
+        </div>
+        <div className="w-[51.82px] h-[51.82px] left-[604.10px] top-0 absolute">
+            <div className="w-[51.82px] h-[51.82px] left-0 top-0 absolute bg-[#eeed00]/0 rounded-full border-4 border-[#eeed00]" />
+            <div className="w-[29.28px] h-[29.28px] left-[11.37px] top-[11.37px] absolute bg-[#eeed00]/0 rounded-full border-4 border-[#eeed00]" />
+        </div>
+        <div className="w-[1541.85px] left-[65.72px] top-[154.19px] absolute"><span class="text-[#222023] text-[30.83px] font-semibold font-['Poppins']">Freelance Graphic Designer<br/></span><span class="text-[#222023] text-[30.83px] font-medium font-['Poppins']">Developed custom graphics, logos, and branding materials for diverse clients.<br/><br/></span><span class="text-[#222023] text-[30.83px] font-semibold font-['Poppins']">Graphic Designer & Animator- Skiome<br/></span><span class="text-[#222023] text-[30.83px] font-medium font-['Poppins']">Contributed to branding assets and created engaging content for digital and print platforms.<br/><br/></span><span class="text-[#222023] text-[30.83px] font-semibold font-['Poppins']">Graphic Design Contractor<br/></span><span class="text-[#222023] text-[30.83px] font-medium font-['Poppins']">Provided design services to local institutions, creating promotional materials and signage</span></div>
+    </div>
+    <div className="w-[1616.42px] h-[194.37px] left-[140.47px] top-[2558.53px] absolute">
+        <div className="w-[1541.85px] left-[60.66px] top-[102.37px] absolute"><span class="text-[#222023] text-[30.83px] font-semibold font-['Poppins']">Indian Institute of Information Technology, Vadodara <br/></span><span class="text-[#222023] text-[30.83px] font-medium font-['Poppins']">B.Tech in Computer Science and Engineering</span></div>
+        <div className="w-[418.32px] left-[1198.09px] top-[89.73px] absolute text-right text-[#222023] text-[30.83px] font-semibold font-['Poppins']"> Ahemdabad, Gujarat<br/>pursuing November 2022</div>
+        <div className="w-[430.96px] h-[76px] left-0 top-0 absolute">
+            <div className="w-[363.98px] h-[22.75px] left-[66.98px] top-[37.91px] absolute bg-[#fef102]" />
+            <div className="left-0 top-0 absolute text-[#222023] text-[62.39px] font-extrabold font-['Inter']">EDUCATION</div>
+        </div>
+    </div>
+    <div className="w-[315.95px] h-[83.41px] left-[819.14px] top-[3321.88px] absolute">
+        {/* <div className="w-[315.95px] h-[83.41px] left-0 top-0 absolute bg-[#202020] rounded-[58.88px] shadow-[0px_14.719711303710938px_6.542093753814697px_0px_rgba(203,203,203,0.41)] border-4 border-white" /> */}
+        {/* <div className="w-[166.82px] h-[31.07px] left-[74.12px] top-[26.39px] absolute text-center text-white text-[26.17px] font-semibold font-['Inter']"><button>HIRE ME</button></div> */}
+    </div>
+    <div className="w-[567.45px] h-[274.25px] left-[217.56px] top-[526.32px] absolute text-white text-2xl font-medium font-['Poppins']">IIT Vadodara CS&E student and President of Student Gymkhana, adept at event management and fostering a dynamic skill environment. Passionate about mastering coding languages, exploring emerging technologies, and driving creative initiatives in the tech community<br/></div>
+    <div className="w-[1091.93px] h-[346.28px] left-[227.67px] top-[2907.35px] absolute">
+        <div className="w-[346.28px] h-[346.28px] left-0 top-0 absolute bg-[#4b4b4b] justify-center items-center inline-flex overflow-hidden">
+            <img className="w-[346.28px] h-[346.28px]" src="hizru2.png" />
+        </div>
+        <div className="w-[346.28px] h-[346.28px] left-[372.83px] top-[-0px] absolute bg-[#4b4b4b] justify-end items-center inline-flex overflow-hidden">
+            <img className="w-[363.98px] h-[514.85px]" src="/hizru1.png" />
+        </div>
+        <div className="w-[346.28px] h-[346.28px] left-[745.65px] top-[-0px] absolute bg-[#4b4b4b] justify-center items-center inline-flex overflow-hidden">
+            <img className="w-[346.28px] h-[346.28px]" src="thala7.png" />
+        </div>
+    </div>
+    <div className="w-[346.28px] h-[346.28px] left-[1346.14px] top-[2907.35px] absolute bg-[#4b4b4b]  overflow-hidden">
+        <img className="w-[623.06px] h-[350.47px] left-[-122.59px] top-[-2.53px] absolute" src="/kirkitnigga.png" />
+        <div className="w-[347.55px] h-[347.55px] left-[-1.26px] top-0 absolute">
+            <div className="w-[347.55px] h-[347.55px] left-0 top-0 absolute bg-[#f7f04b]/75" />
+            <div className="w-[241.39px] h-[36.07px] left-[50.55px] top-[141.55px] absolute text-center text-[#222023] text-[27.39px] font-semibold font-['Inter']">MORE PROJECTS</div>
+            <div className="w-[288.15px] h-[59.40px] left-[30.33px] top-[190.84px] absolute text-center text-[#373737] text-[42.02px] font-semibold font-['Inter'] underline">CLICK ME</div>
+        </div>
+    </div>
+    <div className="w-[898.54px] h-[127.68px] left-[730.74px] top-[1672.60px] absolute">
+        <div className="w-[506.79px] h-[76px] left-[25.20px] top-[-0px] absolute">
+            <div className="w-[447.39px] h-[22.75px] left-[59.40px] top-[37.91px] absolute bg-[#fef102]" />
+            <div className="left-0 top-0 absolute text-[#222023] text-[62.39px] font-extrabold font-['Inter']">CONTACT ME</div>
+        </div>
+        <div className="w-[898.54px] h-[48.23px] pl-[26.34px] pr-[41.38px] pt-[0.65px] pb-[1.59px] left-0 top-[79.45px] absolute justify-start items-center gap-[11.62px] inline-flex overflow-hidden">
+            <div className="w-[781.29px] text-[#222023] text-[30.83px] font-medium font-['Poppins']">vaibhavkumarjonwal@gmail.com</div>
+        </div>
+    </div>
+    <div className="w-[1225.47px] h-[535.86px] left-[763.53px] top-[1132.95px] absolute">
+        <div className="w-[260.35px] h-[84.68px] left-[360.18px] top-0 absolute">
+        </div>
+        <div className="w-[1225.47px] h-[509.32px] left-0 top-[26.54px] absolute">
+            <div className="w-[271.72px] h-[76px] left-0 top-0 absolute">
+                <div className="w-[221.17px] h-[22.75px] left-[50.55px] top-[37.91px] absolute bg-[#fef102]" />
+                <div className="left-0 top-0 absolute text-[#222023] text-[62.39px] font-extrabold font-['Inter']">SKILLS</div>
+            </div>
+            <div className="w-[1129.42px] h-[448.65px] left-[96.05px] top-[60.66px] absolute"><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">Programming Languages:</span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']"> Python, Java, C, JavaScript <br/></span><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">UI/UX: </span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']">Figma, Adobe Illustrator, Adobe Photoshop, Blender <br/></span><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">Game Development: </span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']">Unreal Engine <br/></span><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">Version Control:</span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']"> Git, GitHub <br/></span><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">Animation:</span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']"> Adobe Animate, Adobe Character Animation<br/></span><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">Video Editing:</span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']"> Premier Pro, Wondershare <br/></span><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">Cloud Computing:</span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']"> AWS, Cloud Hosting <br/></span><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">Cyber Security:</span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']"> Android Pentesting, Network Theory, Bug Bounty <br/></span><span class="text-[#222023] text-[27.46px] font-bold font-['Poppins']">Soft Skills:</span><span class="text-[#222023] text-[27.46px] font-medium font-['Poppins']"> Leadership, Management, Teamwork, Communication</span></div>
+        </div>
+    </div>
+    <img className="w-[43.21px] h-[22.10px] left-[510.69px] top-[1228.62px] absolute" src="https://via.placeholder.com/43x22" />
+    <img className="w-[19.15px] h-[8.90px] left-[485.51px] top-[1228.62px] absolute" src="https://via.placeholder.com/19x9" />
+</div>
+  )
 }
